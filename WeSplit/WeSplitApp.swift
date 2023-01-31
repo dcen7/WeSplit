c4d506579e4fa87b8e02ab49078036211e1958f1
dcen7 <mdcengiz@gmail.com>
2023-01-31 10:14:14 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mehmet Deniz Cengiz on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
